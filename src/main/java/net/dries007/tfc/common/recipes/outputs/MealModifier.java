6bb1b810cc9f30f1a720cf2144b24868ef58707d
Ricemonger <leskotr23@gmail.com>
2025-09-26 14:16:41 +0300

    added hunger for sandwiches, that use foods with no hunger values

--- src/main/java/net/dries007/tfc/common/recipes/outputs/MealModifier.java
+++ src/main/java/net/dries007/tfc/common/recipes/outputs/MealModifier.java
@@ -98,6 +98,13 @@ public record MealModifier(FoodData baseFood, List<MealPortion> portions) implem
                 for (Nutrient nutrient : Nutrient.VALUES) {
                     nutrition[nutrient.ordinal()] += data.nutrient(nutrient) * portion.nutrientModifier * item.getCount();
                 }
+
+                int foodHunger = data.hunger();
+
+                if (foodHunger <= 0) {
+                    foodHunger = 4;
+                }
+
                 hunger += data.hunger() * portion.hungerModifier * item.getCount();
                 water += data.water() * portion.waterModifier * item.getCount();
                 saturation += data.saturation() * portion.saturationModifier * item.getCount();
