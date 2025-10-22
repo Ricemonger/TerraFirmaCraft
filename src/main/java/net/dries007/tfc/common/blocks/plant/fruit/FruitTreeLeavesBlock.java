00900573b76ee75c8e43b0f602d056758fbc64d4
Ricemonger <leskotr23@gmail.com>
2025-10-22 10:59:05 +0300

    rebalanced trees fruit chance

--- src/main/java/net/dries007/tfc/common/blocks/plant/fruit/FruitTreeLeavesBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/plant/fruit/FruitTreeLeavesBlock.java
@@ -150,7 +150,7 @@ public class FruitTreeLeavesBlock extends SeasonalPlantBlock implements IForgeBl
 
                 if (range.checkBoth(hydration, Climate.getAverageTemperature(level, pos), false))
                 {
-                    if(currentLifecycle == Lifecycle.FLOWERING&& Math.random() > 0.9f){
+                    if(currentLifecycle == Lifecycle.FLOWERING&& Math.random() > 0.8f){
                         currentLifecycle = currentLifecycle.advanceTowards(expectedLifecycle);
                     }
                 }
